15d434a85f344fc86340c72ca80426f8f7a62e6b
Joseph Son <son.joseph98@gmail.com>
2019-12-29 11:30:19 -0800

    added authentication middleware

--- firebase-functions/functions/handlers/users.js
+++ firebase-functions/functions/handlers/users.js
@@ -1,4 +1,4 @@
-const { db } = require('../util/admin');
+const { db, admin } = require('../util/admin');
 const config = require('../util/config');
 const firebase = require('firebase');
 firebase.initializeApp(config);
@@ -99,5 +99,53 @@ exports.login = (req, res) => {
         } else {
             return res.status(500).json({error: err.code});
         }
+    });
+};
+
+exports.uploadImage = (req, res) => {
+    const BusBoy = require('busboy');
+    const path = require('path');
+    const os = require('os');
+    const fs = require('fs');
+    const busboy = new BusBoy({ headers: req.headers });
+
+    let imageFileName;
+    let imageToBeUploaded = {};
+
+    // create file
+    busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
+
+        console.log(fieldname);
+        console.log(filename);
+        console.log(mimetype);
+
+        // extract file type of image
+        const imageExt = filename.split('.')[filename.split('.').length - 1];
+        imageFileName = `${Math.round(Math.random()*10000000000000)}.${imageExt}`;
+
+        // create file path
+        const filepath = path.join(os.tmpdir(), imageFileName);
+        imageToBeUploaded = { filepath, mimetype };
+        file.pipe(fs.createWriteStream(filepath));
+    });
+
+    // upload file 
+    busboy.on('finish', () => {
+        admin.storage().bucket().upload(imageToBeUploaded.filepath, {
+            resumable: false,
+            metadata: {
+                metadata: {
+                    contentType: imageToBeUploaded.mimetype
+                }
+            }
+        }).then(() => {
+            const imageURL = `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${imageFileName}?alt=media`;
+            return db.doc(`/Users/${req.user.handle}`).update({ imageUrl: imageURL});
+        }).then(() => {
+            return res.json({ message: 'Image uploaded successfully'});
+        }).catch(err => {
+            console.error(err.code);
+            return res.status(500).json({ error: err.code });
+        });
     });
 };
--- firebase-functions/functions/index.js
+++ firebase-functions/functions/index.js
@@ -1,13 +1,15 @@
 // initialize
 const functions = require('firebase-functions');
 const app = require('express')();
+const FirebaseAuth = require('./util/fbAuth');
 
 // get functions from handlers
-const { getAllUsers, signup, login } = require('./handlers/users');
+const { getAllUsers, signup, login, uploadImage } = require('./handlers/users');
 
 // User routes
 app.get('/users', getAllUsers);         // get all uesrs from database
 app.post('/signup', signup);            // sign up: creates user in auth and also creates user in database
 app.post('/login', login);              // login with email and password
+app.post('/user/image', FirebaseAuth, uploadImage);         // upload profile pic for user
 
 exports.api = functions.https.onRequest(app);
